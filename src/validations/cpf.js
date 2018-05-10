ded8e02d5c3f612210816fb1a187d85ab47fc480
Garcez <allan.f.garcez@gmail.com>
2018-05-10 04:26:42 -0300

    Date + Cpf validation

---
+++ src/validations/cpf.js
@@ -0,0 +1,22 @@
+const type = require('./types');
+
+const verify = (rest) => rest == 10 || rest == 11;
+const modRest = (sum) => (sum * 10) % 11;
+
+const cpf = (str) => {
+	if (!type.string) return false;
+	const cpf = str.replace(/-/g, '').replace(/\./g, '');
+	let checkSum = 0;
+	let rest;
+	for (i = 1; i <= 9; i++) checkSum = checkSum + parseInt(cpf.substring(i - 1, i)) * (11 - i);
+	rest = modRest(checkSum);
+	if (verify(rest)) rest = 0;
+	if (rest != parseInt(cpf.substring(9, 10))) return false;
+	checkSum = 0;
+	for (i = 1; i <= 10; i++) checkSum = checkSum + parseInt(cpf.substring(i - 1, i)) * (12 - i);
+	rest = modRest(checkSum);
+	if (verify(rest)) rest = 0;
+	return rest != parseInt(cpf.substring(10, 11)) ? false : true;
+};
+
+module.exports = cpf;
